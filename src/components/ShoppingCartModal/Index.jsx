a7f3024dadd09592e2cf51aa668d1eb661fee9f7
oAndin <00andherson@gmail.com>
2023-11-05 23:45:28 -0300

    fetch working

--- src/components/ShoppingCartModal/Index.jsx
+++ src/components/ShoppingCartModal/Index.jsx
@@ -1,19 +1,39 @@
 
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './index.css'
 import { ShoppingCartContext } from '../../contexts/ShoppingCartContext/Index';
 
 const ShoppingCartModal = () => {
 
     const { isCartOpen } = useContext(ShoppingCartContext);
+    const [itensCarrinho, setItensCarrinhos] = useState([]);
+    useEffect(() => {
 
+        fetch("http://localhost:5000/itensCarrinho", {
+            method: "GET",
+            headers: {
+                "Content-type": "application-json"
+            }
+        }).then((response) => response.json())
+            .then((data) => {
+                setItensCarrinhos(data)
+            })
+            .catch(error => console.log(error))
+
+    }, []);
     return (
         <>
             {isCartOpen && (
                 <span id='shopping-cart-modal'>
                     <h3>Meu carrinho</h3>
                     <span id='line'></span>
-                    <div></div>
+                    {itensCarrinho.map((itemCarrinho) => (
+                        <div key={itemCarrinho.id}>
+                            <h1>{itemCarrinho.name}</h1>
+                            <h1>{itemCarrinho.preco}</h1>
+                            <img src={itemCarrinho.img} alt="imagem do produto" />
+                        </div>
+                    ))}
                     <span id='line'></span>
                 </span >
             )}
