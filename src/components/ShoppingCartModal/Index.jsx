f11868e4e404cd435246a574eb06fa72522dfb0b
oAndin <00andherson@gmail.com>
2023-11-06 03:04:50 -0300

    update items-cart

--- src/components/ShoppingCartModal/Index.jsx
+++ src/components/ShoppingCartModal/Index.jsx
@@ -1,5 +1,5 @@
 
-import { useContext} from 'react'
+import { useContext } from 'react'
 import './index.css';
 import { ShoppingCartContext } from '../../contexts/ShoppingCartContext/Index';
 import ItemsCart from '../ItemsCart/Index';
@@ -14,12 +14,22 @@ const ShoppingCartModal = () => {
                     <h3>Meu carrinho</h3>
                     <span id='line'></span>
                     <div id='carrinho'>
-                    <ItemsCart/>
-                    <ItemsCart/>
-                    <ItemsCart/>
-                    <ItemsCart/>
+                        <ItemsCart />
+                        <ItemsCart />
+                        <ItemsCart />
+                        <ItemsCart />
                     </div>
                     <span id='line'></span>
+                    <span id='checkout'>
+                        <div id='lado-lado'>
+                            <h3 id='total-title'>Valor total:</h3>
+                            <h3 id='total-price'>R$ 00.00</h3>
+                        </div>
+                        <div id='lado-lado'>
+                            <button id='btn-esvaziar'><p>Esvaziar</p></button>
+                            <button id='btn-carrinho'>Ver carrinho</button>
+                        </div>
+                    </span>
                 </span >
             )}
         </>
